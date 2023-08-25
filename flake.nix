8fd656b90f06ca910dd3b1e115370e95974bb10e
Diego Dorado <diegodorado@gmail.com>
2023-08-25 03:50:16 -0300

    add helix configs

--- flake.nix
+++ flake.nix
@@ -108,7 +108,6 @@
             nil #lsp for nix
             # rnix-lsp #another one
             ripgrep
-            helix
             jq
             just
             go
@@ -164,6 +163,81 @@
               }
             '';
 
+          };
+
+          programs.helix = {
+            enable = true;
+            settings = {
+              
+              theme="everforest_dark";
+
+              editor = {
+                 bufferline = "always";
+                 rulers=[80];
+              };
+
+              keys.normal = {
+               space.e = ":sh zellij run --close-on-exit --name 'File Tree' --floating -- ~/.cargo/bin/sidetree";
+               space.h = ":toggle file-picker.hidden";
+              };
+            };
+
+            languages = {
+
+              language = [
+                
+{
+name = "html";
+formatter = { command = "yarn"; args = ["--silent" "prettier" "--parser"  "html" ]; };
+} 
+{
+name = "json";
+formatter = { command = "yarn"; args = ["--silent"  "prettier" "--parser"  "json" ]; };
+} 
+{
+name = "css";
+formatter = { command = "yarn"; args = ["--silent"  "prettier" "--parser"  "css" ]; };
+} 
+{
+name = "javascript";
+formatter = { command = "yarn"; args = ["--silent"  "prettier" "--parser"  "typescript" ]; };
+auto-format = true;
+} 
+{
+name = "typescript";
+formatter = { command = "yarn"; args = ["--silent"  "prettier" "--parser"  "typescript" ]; };
+auto-format = true;
+} 
+{
+name = "tsx";
+formatter = { command = "yarn"; args = ["--silent"  "prettier" "--parser"  "typescript" ]; };
+auto-format = true;
+} 
+{
+name = "bash";
+indent = { tab-width = 4;  unit = "    "; };
+formatter = { command = "shfmt"; args = ["-i"  "4"]; };
+auto-format = true;
+} 
+{
+name = "python";
+formatter = { command = "black"; args = ["--quiet"  "-"]; };
+auto-format = true;
+} 
+{
+name = "ruby";
+formatter = { command = "bundle"; args = ["exec"  "standardrb"  "--fix"  "--stderr"]; };
+auto-format = true;
+# language-server = { command = "standardrb"  args = ["--lsp"] }
+} 
+{
+name = "nix";
+# formatter = { command = "bundle"  args = ["exec"  "standardrb"  "--fix"  "--stderr"] }
+auto-format = true;
+# language-server = { command = "rnix-lsp" }
+}];
+              
+            };
           };
 
           programs.kitty.enable = true;
