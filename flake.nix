12664919623dd0a469bf10aecd9d1368b97b6a94
Diego Dorado <diegodorado@gmail.com>
2023-09-27 22:58:15 -0300

    now do remove helix

--- flake.nix
+++ flake.nix
@@ -174,96 +174,13 @@
             vimAlias = true;
             vimdiffAlias = true;
           };
-            
-          programs.helix = {
-            enable = true;
-            settings = {
 
-              theme = "everforest_dark";
-
-              editor = {
-                bufferline = "always";
-                rulers = [ 80 ];
-              };
-
-              keys.normal = {
-                space.e = ":sh zellij run --close-on-exit --name 'File Tree' --floating -- ~/.cargo/bin/sidetree";
-                space.h = ":toggle file-picker.hidden";
-              };
-            };
-
-            languages = {
-
-              language = [
-
-                {
-                  name = "html";
-                  formatter = { command = "yarn"; args = [ "--silent" "prettier" "--parser" "html" ]; };
-                }
-                {
-                  name = "json";
-                  formatter = { command = "yarn"; args = [ "--silent" "prettier" "--parser" "json" ]; };
-                }
-                {
-                  name = "css";
-                  formatter = { command = "yarn"; args = [ "--silent" "prettier" "--parser" "css" ]; };
-                }
-                {
-                  name = "javascript";
-                  formatter = { command = "yarn"; args = [ "--silent" "prettier" "--parser" "typescript" ]; };
-                  auto-format = true;
-                }
-                {
-                  name = "typescript";
-                  formatter = { command = "yarn"; args = [ "--silent" "prettier" "--parser" "typescript" ]; };
-                  auto-format = true;
-                }
-                {
-                  name = "tsx";
-                  formatter = { command = "yarn"; args = [ "--silent" "prettier" "--parser" "typescript" ]; };
-                  auto-format = true;
-                }
-                {
-                  name = "bash";
-                  indent = { tab-width = 4; unit = "    "; };
-                  formatter = { command = "shfmt"; args = [ "-i" "4" ]; };
-                  auto-format = true;
-                }
-                {
-                  name = "python";
-                  formatter = { command = "black"; args = [ "--quiet" "-" ]; };
-                  auto-format = true;
-                }
-                {
-                  name = "ruby";
-                  # formatter = { command = "bundle"; args = [ "exec" "standardrb" "--fix" "--stderr" ]; };
-                  auto-format = true;
-                  language-server = { command = "standardrb"; args = ["--lsp"]; };
-                }
-                {
-                  name = "nix";
-                  # formatter = { command = "bundle"  args = ["exec"  "standardrb"  "--fix"  "--stderr"] }
-                  auto-format = true;
-                  # language-server = { command = "rnix-lsp" }
-                }
-                {
-                  name = "cpp";
-                  auto-format = true;
-                }
-                {
-                  name = "scss";
-                  auto-format = true;
-                  file-types = ["scss" "sass"];
-                }
-              ];
-
-            };
+          # simple approach: symlink nvim to this repository
+          home.file."./.config/nvim/" = {
+            source = config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/Code/nix-config/nvim";
           };
 
-          programs.kitty.enable = true;
-          # programs.kitty.package = nixGLWrap pkgs.kitty;
-
-
+            
           programs.bat.enable = true;
           programs.bat.config.theme = "TwoDark";
           programs.fzf.enable = true;
@@ -407,11 +324,6 @@
           };
 
           home.file.".inputrc".source = ./modules/home-manager/dotfiles/inputrc;
-
-          # simple approach: symlink nvim to this repository
-          home.file."./.config/nvim/" = {
-            source = config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/Code/nix-config/nvim";
-          };
 
           # Let Home Manager install and manage itself.
           # programs.home-manager.enable = true;
