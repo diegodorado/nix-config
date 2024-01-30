77ea90693f9001f8e91f1fe978c36e5f7833ffaf
Diego Dorado <diegodorado@gmail.com>
2024-01-30 17:41:29 -0300

    restore neotree, but disable fuzzy filter

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,9 +1,5 @@
 -- See `:help vim.o`
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,25 +1,28 @@
--- neotree filter was buggy as hell
--- nvim-tree is less feature complete,
--- but at least searching works ok
 return {
-  'nvim-tree/nvim-tree.lua',
+  'nvim-neo-tree/neo-tree.nvim',
   version = '*',
-  lazy = false,
   dependencies = {
-    'nvim-tree/nvim-web-devicons',
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('nvim-tree').setup {
-      view = {
-        width = 50,
+    require('neo-tree').setup {
+      popup_border_style = 'rounded',
+      window = {
+        width = 40,
+        auto_expand_width = true,
+        mappings = {
+          -- disable fuzzy finder
+          ['/'] = 'noop',
+        },
       },
-      renderer = {
-        group_empty = true,
-      },
-      update_focused_file = {
-        enable = true,
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
       },
     }
   end,
-  vim.keymap.set('n', '<leader>fe', '<Cmd>NvimTreeToggle<CR>'),
+  vim.keymap.set('n', '<leader>fe', '<Cmd>Neotree toggle<CR>'),
 }
