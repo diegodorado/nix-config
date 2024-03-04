14b9279fb849bd3cdd30d4381c0d0f9f7482686d
Diego Dorado <diegodorado@gmail.com>
2024-03-04 14:48:47 -0300

    add maestro

--- flake.nix
+++ flake.nix
@@ -160,6 +160,9 @@
             wormhole-william
             swift-format
 
+            # mobile automation tool
+            maestro
+
             # # It is sometimes useful to fine-tune packages, for example, by applying
             # # overrides. You can do that directly here, just don't forget the
             # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
