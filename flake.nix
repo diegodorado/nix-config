6c42ddd3c85ec6fbe36ecce20e1c1838f3e9518a
Diego Dorado <diegodorado@gmail.com>
2024-06-02 21:36:02 -0300

    add clojure

--- flake.nix
+++ flake.nix
@@ -163,6 +163,9 @@
             # mobile automation tool
             maestro
 
+            leiningen
+            clojure
+
             # # It is sometimes useful to fine-tune packages, for example, by applying
             # # overrides. You can do that directly here, just don't forget the
             # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
