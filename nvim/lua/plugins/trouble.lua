299a9438bf302e50c715bb24af86cad94cc32da5
Diego Dorado <diegodorado@gmail.com>
2024-06-08 12:53:12 -0300

    fix trouble toggle

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -4,9 +4,9 @@ return {
   cmd = { 'Trouble' },
   opts = { use_diagnostic_signs = true },
   keys = {
-    { '<leader>xx', '<cmd>Trouble diagnostics<cr>', desc = 'Document Diagnostics (Trouble)' },
-    { '<leader>xL', '<cmd>Trouble loclist<cr>', desc = 'Location List (Trouble)' },
-    { '<leader>xQ', '<cmd>Trouble quickfix<cr>', desc = 'Quickfix List (Trouble)' },
+    { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Document Diagnostics (Trouble)' },
+    { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+    { '<leader>xQ', '<cmd>Trouble quickfix toggle<cr>', desc = 'Quickfix List (Trouble)' },
     {
       '[q',
       function()
